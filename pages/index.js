f6c7f8cabf00090e5f0513a75f4d246efab69c63
Omar Waseem <omarwaseem@berkeley.edu>
2023-04-11 04:19:40 -0500

    added typeform

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const typeformUrl = "https://your-typeform-link-here";
+  const typeformUrl = "https://1aps2l5da57.typeform.com/to/tpgvDnhX";
 
   return (
     <div className={styles.container}>
