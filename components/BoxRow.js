e05608b6e661b8a70918bac8bd5a16ddb1e7a853
Omar Waseem <omarwaseem@berkeley.edu>
2023-04-12 21:23:53 -0500

    added images

--- components/BoxRow.js
+++ components/BoxRow.js
@@ -1,23 +1,38 @@
 import React from "react";
 import styles from "../styles/BoxRow.module.css";
+import Image from 'next/image'
 
 function BoxRow() {
   return (
-    <div className={styles.row}>
-      <div className={styles.box}>
-        <p className={styles.boxText}>Hackathon</p>
-        <p className={styles.boxSubtext}>Unlock your creativity and push the boundaries
- of AI at our exciting hackathon event.</p>
-      </div>
-      <div className={styles.box}>
-        <p className={styles.boxText}>Summit</p>
-        <p className={styles.boxSubtext}>Join top experts and explore the latest in AI
- at the San Francisco Summit.</p>
-      </div>
-      <div className={styles.box}>
-        <p className={styles.boxText}>Events</p>
-        <p className={styles.boxSubtext}>Discover the future of AI through expert talks
- and hands-on experiences at SF's AI events.</p>
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.box}>
+          <div className={styles.boxContent}>
+            <div className={styles.boxImage}>
+              <Image src="/keyboard.svg" alt="Keyboard" width={32} height={32} />
+            </div>
+            <p className={styles.boxText}>Hackathon</p>
+          </div>
+          <p className={styles.boxSubtext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.boxContent}>
+            <div className={styles.boxImage}>
+              <Image src="/keyboard.svg" alt="Keyboard" width={32} height={32} />
+            </div>
+            <p className={styles.boxText}>Summit</p>
+          </div>
+          <p className={styles.boxSubtext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className={styles.box}>
+          <div className={styles.boxContent}>
+            <div className={styles.boxImage}>
+              <Image src="/keyboard.svg" alt="Keyboard" width={32} height={32} />
+            </div>
+            <p className={styles.boxText}>Events</p>
+          </div>
+          <p className={styles.boxSubtext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
       </div>
     </div>
   );
