805b462a76a984a8bee8c639bad68a92cdb752f8
Omar Waseem <omarwaseem@berkeley.edu>
2023-04-14 17:16:42 -0700

    added og image and updated desc

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,20 @@ export default function Home() {
     <div className={styles.container}>
       <DefaultSeo
         title="AI SF | May 15-19"
-        description="This isn’t your usual conference. May 15-19. More details announced soon."
+        description="May 15 - 19. An AI week for builders."
         openGraph={{
           url: "https://aisf.co",
           title: "AI SF | May 15-19",
           description:
-            "This isn’t your usual conference. May 15-19. More details announced soon.",
-          // images: [
-          //   {
-          //     url: "https://aisf.co/preview-image.png",
-          //     width: 1200,
-          //     height: 630,
-          //     alt: "Preview Image",
-          //   },
-          // ],
+            "May 15 - 19. An AI week for builders.",
+          images: [
+            {
+              url: "https://aisf.co/preview-image.png",
+              width: 1200,
+              height: 630,
+              alt: "Preview Image",
+            },
+          ],
           site_name: "AI SF",
         }}
       />
