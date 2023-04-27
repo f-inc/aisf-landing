c597a97d933e0fd812aad7ebf0ec7abff343f3ed
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-04-28 03:33:51 +0530

    added SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Marquee from "@/components/Marquee";
 import Hero from "@/components/Hero";
 import Info from "@/components/Info";
+import SEO from "@/components/SEO";
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ const Home = () => {
     <div
       className={`${jakarta.className} flex justify-center w-full bg-[url("/images/background.png")] bg-cover min-h-screen`}
     >
+      <SEO />
       <div className="max-w-screen-2xl w-full">
         <Header />
         <Marquee />
