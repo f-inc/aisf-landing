401ab862022b4ca5aa89a0569b324f99e85bc207
Omar Waseem <omarwaseem@berkeley.edu>
2023-04-29 21:28:30 -0500

    fixed copy

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -7,7 +7,7 @@ const Info = () => {
   return (
     <div className="mt-20 lg:mt-28 border-t w-full h-36 border-white/[.12] grid grid-cols-1 lg:grid-cols-3">
       <div className="w-full p-10 flex gap-x-5 items-start">
-        <Image src={Keyboard} alt="Keyboard" />
+        <Image src={Keyboard} alt="Hackathon" width={30}/>
         <div className="-mt-1">
           <div className="font-semibold text-xl">Hackathon</div>
           <div className="opacity-75 mt-2">
@@ -16,10 +16,10 @@ const Info = () => {
           </div>
         </div>
       </div>
-      <div className="w-full border-y lg:border-x lg:border-y-0 border-white/[.12] p-10 flex gap-x-5 items-start">
-        <Image src={Summit} alt="Keyboard" />
+      <div className="w-full p-10 flex gap-x-5 items-start">
+        <Image src={Summit} alt="Summit" width={30}/>
         <div className="-mt-1">
-          <div className="font-semibold text-xl">Hackathon</div>
+          <div className="font-semibold text-xl">Summit</div>
           <div className="opacity-75 mt-2">
             Join top experts and explore the latest in AI at the San Francisco
             Summit.
@@ -27,7 +27,7 @@ const Info = () => {
         </div>
       </div>
       <div className="w-full p-10 flex gap-x-5 items-start">
-        <Image src={Events} alt="Keyboard" />
+        <Image src={Events} alt="Events" width={30}/>
         <div className="-mt-1">
           <div className="font-semibold text-xl">Events</div>
           <div className="opacity-75 mt-2">
