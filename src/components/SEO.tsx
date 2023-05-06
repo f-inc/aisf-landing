381c3394c48fac169891829c8c425c745d7eea55
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-05-07 04:57:01 +0530

    linktree analytics

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -25,16 +25,18 @@ const SEO = () => {
 
       <Script strategy="lazyOnload">{`
         _linkedin_partner_id = "4782732";
-window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-  (function(l) {
-if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-window.lintrk.q=[]}
-var s = document.getElementsByTagName("script")[0];
-var b = document.createElement("script");
-b.type = "text/javascript";b.async = true;
-b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-s.parentNode.insertBefore(b, s);})(window.lintrk);
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        (function(l) {
+        if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+        window.lintrk.q=[]}
+        var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script");
+        b.type = "text/javascript";b.async = true;
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+        s.parentNode.insertBefore(b, s);})(window.lintrk);
+
+        window.lintrk('track', { conversion_id: 12231860 });
       `}</Script>
     </>
   );
