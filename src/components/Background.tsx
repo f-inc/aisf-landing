9904dd003619cce10b50ac7b18082de50bcc7ef1
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-05-12 15:48:52 +0530

    background height long viewport

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,6 @@
 const Background = () => {
   return (
-    <div className="inset-0 fixed -z-10 w-screen h-screen bg-black bg-[url('/images/background-small.png')] lg:bg-[url('/images/background.png')] bg-cover bg-center"></div>
+    <div className="inset-0 fixed -z-10 w-screen h-[100lvh] bg-black bg-[url('/images/background-small.png')] lg:bg-[url('/images/background.png')] bg-cover bg-center"></div>
   );
 };
 
