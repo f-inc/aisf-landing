7da4ac4ca153c614d13a89892a024b88aad63970
Hulkhere <88937554+hulkhere@users.noreply.github.com>
2023-05-25 21:37:25 +0530

    add: tweet

--- src/components/Rewind.tsx
+++ src/components/Rewind.tsx
@@ -1,17 +1,19 @@
 import RewindImg from "images/rewind/rewind.png";
 import Image from "next/image";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const Rewind = () => {
   return (
     <>
-      <div id="rewind2023" className="flex flex-col justify-center items-center gap-y-10 lg:gap-y-16 pt-28 lg:pt-40">
-        <div className="font-bold font-feature text-4xl lg:text-7xl">2023 Rewind</div>
-        <div className="p-5 lg:w-[420px] bg-white/[.05] rounded-lg border border-white/[.1] mx-6">
-          <Image
-            src={RewindImg}
-            alt="Emmanuel"
-            className="lg:w-[420px] lg:h-[300px] rounded-lg"
-          />
+      <div
+        id="rewind2023"
+        className="flex flex-col justify-center items-center gap-y-10 lg:gap-y-16 pt-28 lg:pt-40"
+      >
+        <div className="font-bold font-feature text-4xl lg:text-7xl">
+          2023 Rewind
+        </div>
+        <div className="p-5 w-80 lg:w-[420px] bg-white/[.05] rounded-lg border border-white/[.1] mx-6 lg:mx-0">
+          <TwitterTweetEmbed tweetId="1660765392581840896" />
         </div>
       </div>
     </>
